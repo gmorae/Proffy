4710b20ed14a55a6e713a74bb22026c4007d1cd7
gmorae <gmmartins06@gmail.com>
2020-08-08 04:46:23 -0300

    Fixing the loading page

--- web/src/components/Loading/index.tsx
+++ web/src/components/Loading/index.tsx
@@ -5,9 +5,12 @@ import './style.css';
 const Loading: React.FC = () => {
   return (
     <main className="background-success">
-      <div className="lds-dual-ring"></div>
-      <h1>Carregando . . .</h1>
-      <p>Estámos preparando tudo para você</p>
+      <div className="container">
+
+        <div className="lds-dual-ring"></div>
+        <h1>Carregando . . .</h1>
+        <p>Estámos preparando tudo para você</p>
+      </div>
     </main>
   )
 }
